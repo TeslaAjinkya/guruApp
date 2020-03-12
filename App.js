9f33243963062f3a2dcf07dd8508abedafed61bb
Akshayagt20 <62073262+Akshayagt20@users.noreply.github.com>
2020-03-12 13:12:17 +0530

    aSas
    aAs

--- App.js
+++ App.js
@@ -35,7 +35,7 @@ const App: () => React$Node = () => {
           <Header />
           {global.HermesInternal == null ? null : (
             <View style={styles.engine}>
-              <Text style={styles.footer}>AKSHAYA</Text>
+              <Text style={styles.footer}>AKSHAYA123ss</Text>
             </View>
           )}
           <View style={styles.body}>
